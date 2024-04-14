2442ddb73c1bc3256e903ae08f9e9519736ac37b
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-04-14 23:36:08 +0300

    feat: add how it works section

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,18 +2,27 @@ import ResponsiveAppBar from "../components/ResponsiveAppBar";
 import Box from '@mui/material/Box';
 import image1 from "../images/image1.jpg";
 import { Button, Typography, CardActionArea, CardActions } from "@mui/material";
-import { GifBoxTwoTone } from "@mui/icons-material";
 import Footer from "../components/Footer";
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import sdg2 from "../images/sdg2.png";
 import sdg12 from "../images/sdg12.png";
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import { IconButton } from '@mui/material';
+import delivery from "../images/delivery.png";
+import food from "../images/food.png";
+import reach_out from "../images/reach_out.png";
+import signup from "../images/signup.png";
+import verified from "../images/verified.png";
+
+
 
 const LandingPage = () =>{
     return(
         <>
         <ResponsiveAppBar/>
+        {/* hero section */}
         <Box display="flex" alignItems="center">
         <Box display="flex" justifyContent="space-between" alignItems="left" flexDirection="column">
             <Typography variant = "h2" align = "center" color = "primary" mt = {5}>
@@ -40,6 +49,90 @@ const LandingPage = () =>{
       <img src={image1} alt="feeding the hungry" style={{ width: "500px", height: "500px" }}/>
     </Box>
     </Box>
+    {/* how it works section */}
+    <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column" sx={{mt:10, mb:10}}>
+      <Typography variant="h4" gutterBottom sx={{mt:5, mb:5}}>
+        How it works
+      </Typography>
+      <Box display="flex" alignItems="center">
+        <Card sx={{ maxWidth: 345, width: '20%', margin: 1 }}>
+          <CardMedia
+            component="img"
+            height="140"
+            image={signup}
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Sign up
+            </Typography>
+          </CardContent>
+        </Card>
+        <IconButton color="primary" disabled>
+          <ArrowForwardIcon />
+        </IconButton>
+        <Card sx={{ maxWidth: 345, width: '20%', margin: 1 }}>
+          <CardMedia
+            component="img"
+            height="140"
+            image={verified}
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Get verified
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card sx={{ maxWidth: 345,width: '20%', margin: 1 }}>
+          <CardMedia
+            component="img"
+            height="140"
+            image={reach_out}
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Reach out to food providers
+            </Typography>
+          </CardContent>
+        </Card>
+        <IconButton color="primary" disabled>
+          <ArrowForwardIcon />
+        </IconButton>
+        <Card sx={{ maxWidth: 345, width: '20%', margin: 1 }}>
+          <CardMedia
+            component="img"
+            height="140"
+            image={delivery}
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Collect food from providers
+            </Typography>
+          </CardContent>
+        </Card>
+        <IconButton color="primary" disabled>
+          <ArrowForwardIcon />
+        </IconButton>
+        <Card sx={{ maxWidth: 345,width: '20%', margin: 1 }}>
+          <CardMedia
+            component="img"
+            height="140"
+            image={food}
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+                Distribute food
+            </Typography>
+          </CardContent>
+        </Card>
+      </Box>
+    </Box>
+
+    {/* Goals section */}
     <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column">
       <Typography variant="h4" gutterBottom>
         Goals
