05f607a6e3d8388ea1de0de945637c641b044c14
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-04-14 22:57:18 +0300

    feat: add goals section to landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,14 @@
 import ResponsiveAppBar from "../components/ResponsiveAppBar";
 import Box from '@mui/material/Box';
 import image1 from "../images/image1.jpg";
-import { Button, Typography } from "@mui/material";
+import { Button, Typography, CardActionArea, CardActions } from "@mui/material";
 import { GifBoxTwoTone } from "@mui/icons-material";
 import Footer from "../components/Footer";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import sdg2 from "../images/sdg2.png";
+import sdg12 from "../images/sdg12.png";
 
 const LandingPage = () =>{
     return(
@@ -34,6 +39,49 @@ const LandingPage = () =>{
     >
       <img src={image1} alt="feeding the hungry" style={{ width: "500px", height: "500px" }}/>
     </Box>
+    </Box>
+    <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column">
+      <Typography variant="h4" gutterBottom>
+        Goals
+      </Typography>
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <Card sx={{ maxWidth: 345, margin: 10, mr: 20 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="300"
+              image= {sdg2}
+              alt="Sustainable Development Goal 2"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+              Sustainable Development Goal 2
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+            FoA aims to achieve the goal of zero hunger by providing food to the less fortunate.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+        <Card sx={{ maxWidth: 345, margin: 1 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="300"
+              image={sdg12}
+              alt="Sustainable Development Goal 12"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+              Sustainable Development Goal 12
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                FoA aims to achieve the goal of responsible consumption and production by reducing food wastage.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Box>
     </Box>
 
     <Footer/>
